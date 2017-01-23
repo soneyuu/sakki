d65e04798e41bf49e54b764fef0db70a9ffab6bf
曽根優希 <Yuuki-Sone@soneyu.local>
2017-01-23 14:46:52 +0900

    Implement entry create and read

--- app.rb
+++ app.rb
@@ -2,6 +2,14 @@ require "bundler/setup"
 Bundler.require(:default)
 require "sinatra/reloader"
 
+Dir["models/*.rb"].each do |model|
+  require_relative model
+end
+
+Dir["repositories/*.rb"].each do |repository|
+  require_relative repository
+end
+
 class App < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
@@ -11,7 +19,33 @@ class App < Sinatra::Base
     set :views, settings.root + "/views"
   end
 
+  helpers do
+    def entry_repository
+      @@entry_repository ||= EntryRepository.new
+    end
+  end
+
   get "/" do
     slim :index
+  end
+
+  get "/entries/new" do
+    slim :new
+  end
+
+  post "/entries" do
+    entry = Entry.new
+    entry.title = params[:title]
+    entry.body = params[:body]
+    id = entry_repository.save(entry)
+
+    redirect to("/entries/#{id}")
+  end
+
+  get "/entries/:id" do
+    @entry = entry_repository.fetch(params[:id].to_i)
+    p @entry
+
+    slim :entry
   end
 end
